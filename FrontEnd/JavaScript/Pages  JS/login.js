a9610d869a5e6bbbc4e678641cb3ee63f0977f49
Abdotarek442 <114480959+Abdotarek442@users.noreply.github.com>
2025-11-09 23:58:57 +0200

    Refactoring Dev branch

--- FrontEnd/JavaScript/Pages  JS/login.js
+++
@@ -1,6 +0,0 @@
-document.querySelector("#loginForm").addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const email = document.querySelector("#email").value;
-  const password = document.querySelector("#password").value;
-  await handleLogin(email, password);
-});
